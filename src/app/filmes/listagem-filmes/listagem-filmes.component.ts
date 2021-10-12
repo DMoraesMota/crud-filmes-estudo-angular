26cfdf7c854ffd9cb082d0478519d4db9b4d26d1
Diego Mota <diego_mota@carrefour.com>
2021-10-12 13:53:16 -0300

    Form de pesquisa dinamico

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -5,6 +5,8 @@ import { Router } from '@angular/router';
 import { FilmesService } from 'src/app/core/filmes.service';
 import { ConfigPrams } from 'src/app/shared/models/config-params';
 import { Filme } from 'src/app/shared/models/filme';
+import { debounceTime } from 'rxjs/operators';
+
 
 @Component({
   selector: 'dio-listagem-filmes',
@@ -28,6 +30,25 @@ export class ListagemFilmesComponent implements OnInit {
 
   ngOnInit() {
     this.listarFilmes();
+
+    this.filtrosListagem = this.fb.group({
+      texto: [''],
+      genero: ['']
+    });
+
+    this.filtrosListagem.get('texto').valueChanges
+      .pipe(debounceTime(400))
+      .subscribe( (val: string) => {
+        this.config.pesquisa = val;
+        this.resetarConsulta();
+      });
+
+      this.filtrosListagem.get('genero').valueChanges.subscribe( (val:string) => {
+        this.config.campo = {'tipo': 'genero', valor:val};
+        this.resetarConsulta();
+      });
+
+    this.generos = ['Ação', 'Romance', 'Terror', 'Ficção cientifica', 'Comédia', 'Aventura', 'Drama'];
   }
 
   onScroll(): void {
