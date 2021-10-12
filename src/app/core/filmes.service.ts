c18f97a21d91f81f055406bbfe6f9a375719e81f
Diego Mota <diego_mota@carrefour.com>
2021-10-12 13:24:32 -0300

    Criação do infinte scroll

---
+++ src/app/core/filmes.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { ConfigPrams } from "../shared/models/config-params";
+import { Filme } from "../shared/models/filme";
+import { ConfigParamsService } from "./config-params.service";
+
+const url = 'http://localhost:3000/filmes/';
+
+@Injectable({
+  providedIn: "root",
+})
+export class FilmesService {
+  constructor(
+    private http: HttpClient,
+    private configService: ConfigParamsService
+  ) {}
+
+  listar(config: ConfigPrams): Observable<Filme[]> {
+    const configPrams = this.configService.configurarParametros(config);
+    return this.http.get<Filme[]>(url, { params: configPrams });
+  }
+}
--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,5 +1,10 @@
 import { ValidarCamposService } from './../../shared/components/campos/validar-campos.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FilmesService } from 'src/app/core/filmes.service';
+import { ConfigPrams } from 'src/app/shared/models/config-params';
+import { Filme } from 'src/app/shared/models/filme';
 
 @Component({
   selector: 'dio-listagem-filmes',
@@ -8,13 +13,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListagemFilmesComponent implements OnInit {
 
-  constructor() { }
+  readonly semFoto = 'https://www.termoparts.com.br/wp-content/uploads/2017/10/no-image.jpg';
+  config: ConfigPrams = {
+    pagina: 0,
+    limite: 4
+  };
+  filmes: Filme[] = [];
+  filtrosListagem: FormGroup;
+  generos: Array<string>;
+
+  constructor(private filmesService: FilmesService,
+              private fb: FormBuilder,
+              private router: Router) { }
 
   ngOnInit() {
+    this.listarFilmes();
+  }
 
+  onScroll(): void {
+    console.log('passou');
+
+    this.listarFilmes();
   }
 
   open() {
+
+  }
+
+  private listarFilmes(): void {
+    this.config.pagina++;
+    this.filmesService.listar(this.config)
+      .subscribe((filmes: Filme[]) => this.filmes.push(...filmes));
+  }
+
+  private resetarConsulta(): void {
+    this.config.pagina = 0;
+    this.filmes = [];
+    this.listarFilmes();
   }
 
 }
